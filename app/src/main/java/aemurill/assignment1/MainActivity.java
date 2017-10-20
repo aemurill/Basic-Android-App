e2d9af03e524f746ff18593f18afbb45a6fa7093
aemurill <aemurill@ucsc.edu>
2017-10-19 23:10:04 -0700

    Cleaning up empty comments

--- app/src/main/java/aemurill/assignment1/MainActivity.java
+++ app/src/main/java/aemurill/assignment1/MainActivity.java
@@ -26,13 +26,7 @@ public class MainActivity extends AppCompatActivity{
         myEnterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //text_1 = edit_text_1.getText().toString();
-
                 Intent intent = new Intent(MainActivity.this, EditActivity.class);
-                ////intent.putExtra("Text 2", edit_text_2.getText().toString());
-                //Bundle bundle = new Bundle();
-                //bundle.putString("Text 2", edit_text_2.getText().toString());
-                //intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
@@ -40,20 +34,8 @@ public class MainActivity extends AppCompatActivity{
         myViewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                Toast.makeText(getApplication(),"i am cliked", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this, ViewActivity.class);
-                ////intent.putExtra("Text 2", edit_text_2.getText().toString());
-                //Bundle bundle = new Bundle();
-                //bundle.putString("Text 2", edit_text_2.getText().toString());
-                //intent.putExtras(bundle);
                 startActivity(intent);
-
-
-//                ListViewAdapter adapter=new ListViewAdapter(DisplayAllRouteDetailActivity.this, list);
-//                _display_all_routes_details.setAdapter(adapter);
-
             }
         });
 
